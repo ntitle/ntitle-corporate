0ad20dfdc984076400c0fc376697e441a1052314
Lukasz Ostrowski <lukasz@ostrowski.com.pl>
2018-01-04 17:34:44 +0100

    changed template extension

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ var nunjucksRender = require('gulp-nunjucks-render');
 var data = require('gulp-data');
 
 function getDataForFile(file) {
-    var dataFileName = file.relative.replace('.html', '.data.js');
+    var dataFileName = file.relative.replace('.njk', '.data.js');
     var dataPath = file.path.replace(file.relative, dataFileName);
 
     return require(dataPath);
 }
 
 gulp.task('default', function () {
-    return gulp.src('src/templates/*.html')
+    return gulp.src('src/templates/*.njk')
         .pipe(data(getDataForFile))
         .pipe(nunjucksRender({
             path: 'src/partials'
