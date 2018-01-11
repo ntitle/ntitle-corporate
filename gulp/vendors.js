8b9e3f0146eb087767b2dc48a2a2277f90a0fd03
Lukasz Ostrowski <lukasz@ostrowski.com.pl>
2018-01-11 15:40:54 +0100

    Some vendor change

--- gulp/vendors.js
+++ gulp/vendors.js
@@ -3,7 +3,7 @@ const concat = require('gulp-concat');
 
 
 module.exports = () => {
-    return gulp.src('src/vendors/*.js')
+    return gulp.src(['src/vendors/_jquery-3.2.1.min.js', 'src/vendors/*.js'])
         .pipe(concat('vendors.js'))
         .pipe(gulp.dest('dist/js'));
 };
