3af622eb547090dc07c00b895b50248d997fb758
Lukasz Ostrowski <lukasz@ostrowski.com.pl>
2018-01-11 12:32:28 +0100

    updated Peters bio

--- src/templates/data/team.data.js
+++ src/templates/data/team.data.js
@@ -2,10 +2,10 @@ module.exports = [
     {
         name: 'Peter van Grinsven',
         displayName: 'Peter van<br/>Grinsven',
-        description: 'After receiving his Masters Degree in International Management from Maastricht University in the Netherlands, Peter went on to work for PWC (PricewaterhouseCoopers), specialising in internet business consultancy. The internet has been his\n passion, ever since. Over the past 15 years Peter has built up a strong background in digital consulting and outsourcing. His ultimate goal is to play an active part in the improvements that the internet brings and make a genuine contribution.',
+        description: 'Peter is a serial entrepreneur. After receiving his Masters Degree from Maastricht University, he started at PWC as an internet business consultant. Operating between strategy and technology has been his passion ever since. His company, CONNAXIS, which has offices in 6 locations, creates magical new experiences via innovative technologies. In 2009, he launched Guerra Creativa, a successful creative crowdsourcing marketplace. CONNAXIS has focused on blockchain technologies since 2015.',
         linkedin: 'https://www.linkedin.com/in/connaxisstaycompetitive/',
         avatar: '/img/team/peter.jpg',
-        role: 'CEO'
+        role: 'CEO',
     },
     {
         name: 'Pawel Pinio',
@@ -13,7 +13,7 @@ module.exports = [
         description: 'Paweł is a senior software engineer with extensive experience in building fault tolerant back-end systems. He started his career as a full stack developer before moving to work on diagnostics and radiology information systems. He then moved into interdisciplinary consulting which allowed him to build transformative platforms. Paweł is a strong evangelist of distributed systems, blockchain and decentralization and a firm technical enthusiast of the Ethereum platform and community.',
         linkedin: 'https://www.linkedin.com/in/pawelpinio/',
         avatar: '/img/team/pawel.jpg',
-        role: 'CTO'
+        role: 'CTO',
     },
     {
         name: 'John Clancy',
@@ -21,7 +21,7 @@ module.exports = [
         description: 'John Clancy has over 20 years experience in defining, developing and implementing innovative marketing, PR and sales strategies for new and established companies. His clients are based in Europe, the Middle East and North and South America.\n He is also a marketing and business development lecturer at Inholland University in the Netherlands, visiting professor at the University of Bari Aldo Moro, serial entrepreneur, app developer and author of two books.',
         linkedin: 'https://www.linkedin.com/in/johndavidclancy/',
         avatar: '/img/team/john.jpg',
-        role: 'CMO'
+        role: 'CMO',
     },
     {
         name: 'Gonzalo Balabasquer',
@@ -29,7 +29,7 @@ module.exports = [
         description: 'Gonzalo is a systems engineer who started his professional career as a PHP programmer. In the years that followed, he led a team of developers at a fast-paced agency located in Argentina. Recently, he followed his passion and returned to a technical position to leverage his IT experience as a full stack blockchain developer participating as an active developer of the MakerDao project.',
         linkedin: 'https://www.linkedin.com/in/gbalabasquer/',
         avatar: '/img/team/gonzalo.jpg',
-        role: 'Blockchain Engineer'
+        role: 'Blockchain Engineer',
     },
     {
         name: 'Bastien Hugon',
@@ -37,7 +37,7 @@ module.exports = [
         description: 'Bastien is a project manager with excellent strategic and operational skills. He has leadership competencies for team building, client relations, negotiation, decision making, problem solving and conflict management in French, Spanish and English. He has 10+ years of experience in QA, IT infrastructure and service delivery working with companies like HCL, DANA, Ford, L’Oreal, Nokia and bioMerieux.',
         linkedin: 'https://www.linkedin.com/in/bastienhugon/',
         avatar: '/img/team/bastien.jpg',
-        role: 'Project Manager'
+        role: 'Project Manager',
     },
     {
         name: 'Marcin Gierlak',
@@ -45,6 +45,6 @@ module.exports = [
         description: 'Marcin has a strong background in software engineering including cloud, mobile platforms, cryptography and security, system level programming, and systems development life cycle. He has experience in developing and implementing concepts and technologies for IoT, hands-on experience with Ethereum/Solidity, blockchain technologies, distributed ledger, cryptocurrencies and Smart Contracts.',
         linkedin: 'https://www.linkedin.com/in/marcingierlak/',
         avatar: '/img/team/marcin.jpg',
-        role: 'Blockchain Engineer'
-    }
+        role: 'Blockchain Engineer',
+    },
 ];
