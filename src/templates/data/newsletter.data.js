57181275beb899a918c4fd039316ccda7811d48d
Lukasz Ostrowski <lukasz@ostrowski.com.pl>
2018-01-30 10:48:59 +0100

    Participate page (#23)
    * Cache reverted
    
    * Footer copyright text update
    
    * Added participate page with form (#22)
    
    * Added g-to-homescreen button to participate page
    
    * Participate styles improvements
    
    * Participate style improvement-padding

--- src/templates/data/newsletter.data.js
+++ src/templates/data/newsletter.data.js
@@ -1,4 +1,6 @@
 module.exports = {
     headline: 'KEEP UP-TO-DATE WITH <span class="en">n</span>TITLE’S PROGRESS',
     subtext: 'Great things come to those that sign up (periodic updates, investor info, previews, special offers and much more)',
+    buttonText: 'Sign up',
+    action: 'https://nTitle.us17.list-manage.com/subscribe/post-json?u=38836b86bdc2b88a242addc37&amp;id=048dc960ca',
 };
