55e3902eada3581c3b9991d35c5b42c1f4c58e59
Lukasz Ostrowski <lukasz@ostrowski.com.pl>
2018-01-30 11:17:05 +0100

    Url fix

--- src/templates/participate.data.js
+++ src/templates/participate.data.js
@@ -9,7 +9,7 @@ const data = {
         headline: 'INVEST IN NTITLE AND HELP US<br/>DISRUPT THE GAMING INDUSTRY',
         subtext: 'ENTER YOUR EMAIL TO RECEIVE INVESTOR INFORMATION',
         buttonText: 'Request info',
-        action: 'https://nTitle.us17.list-manage.com/subscribe/post?u=38836b86bdc2b88a242addc37&amp;id=55e367ea0f',
+        action: 'https://nTitle.us17.list-manage.com/subscribe/post-json?u=38836b86bdc2b88a242addc37&amp;id=55e367ea0f',
     },
 };
 
