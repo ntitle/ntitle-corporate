87f9942c5b3794908509c57a2c2a96060f793973
sajTempler <sajtempler@gmail.com>
2018-06-25 12:29:31 +0200

    media logos

--- src/components/SectionNews/SectionNews.data.js
+++ src/components/SectionNews/SectionNews.data.js
@@ -12,51 +12,51 @@ module.exports = [
     {
         title: 'Blockchain Expo Europe',
         url: '#',
-        image: '/img/media/blockchain_expo.png',
+        image: '/img/media/blockchain_expo_europe.png',
     },
     {
         title: 'Dagvandecrypto',
         url: 'https://www.youtube.com/watch?v=zpevvx3VKhI',
-        image: '/img/media/blockchain_expo.png',
+        image: '/img/media/dagvancrypto.png',
     },
     {
         title: 'Angel Launch',
         url: 'https://www.youtube.com/watch?v=J3gD84OfLNg',
-        image: '/img/media/blockchain_expo.png'
+        image: '/img/media/angellaunch.png'
     },
     {
         title: 'Chainges',
         url: 'https://www.youtube.com/watch?v=8kj5KPdrTuk',
-        image: '/img/media/blockchain_expo.png'
+        image: '/img/media/chainges.png'
     },
     {
         title: 'Bitcoin Magazine NL',
         url: 'http://bitcoinmagazine.nl/en/2018/04/interview-vergeet-steam-de-nederlandse-blockchainstartup-ntitle-geeft-de-macht-terug-aan-de-game-ontwikkelaar-en-gamer/',
-        image: '/img/media/blockchain_expo.png'
+        image: '/img/media/bcmag.png'
     },
     {
         title: 'CryptoAcademy',
         url: 'https://www.youtube.com/watch?v=34Luaslg8hs',
-        image: '/img/media/blockchain_expo.png'
+        image: '/img/media/ca-nl.png'
     },
     {
         title: 'DailyHODL',
         url: 'https://dailyhodl.com/2018/02/16/what-is-ntitle/',
-        image: '/img/media/blockchain_expo.png'
+        image: '/img/media/the-daily-hodl-logo.png'
     },
     {
         title: 'Brave New World',
         url: 'https://medium.com/@ntitle/ntitle-progress-update-may-24th-2977454d3ce6',
-        image: '/img/media/blockchain_expo.png'
+        image: '/img/media/bnw.png'
     },
     {
         title: 'Impact Tech',
         url: 'https://www.youtube.com/watch?v=HEC2Jw8hJB0',
-        image: '/img/media/blockchain_expo.png'
+        image: '/img/media/impacttech.png'
     },
     {
         title: 'Dragon Valley',
         url: 'https://www.youtube.com/watch?v=MKtkBhtTYHA',
-        image: '/img/media/blockchain_expo.png'
+        image: '/img/media/dragonvalley.png'
     },
 ];
