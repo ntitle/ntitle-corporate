8b3bf7f3625dcbd406a2c1c0778d35b03fe2819b
Mark Vonk <markjhvonk@gmail.com>
2018-08-13 12:10:27 +0200

    Split news and events

---
+++ src/components/SectionEvents/SectionEvents.data.js
@@ -0,0 +1,52 @@
+module.exports = [
+    {
+        title: 'Game Developer Conference',
+        url: 'https://www.youtube.com/watch?v=YBijFXQFZrc',
+        image: '/img/media/gdc_logo.png',
+    },
+    {
+        title: 'Blockchain Expo Global',
+        url: 'https://www.youtube.com/watch?v=yJ36deomX5c',
+        image: '/img/media/blockchain_expo_global.png',
+    },
+    {
+        title: 'Blockchain Expo Europe',
+        url: '#',
+        image: '/img/media/blockchain_expo_europe.png',
+    },
+    {
+        title: 'Dagvandecrypto',
+        url: 'https://www.youtube.com/watch?v=zpevvx3VKhI',
+        image: '/img/media/dagvancrypto.png',
+    },
+    {
+        title: 'Angel Launch',
+        url: 'https://www.youtube.com/watch?v=J3gD84OfLNg',
+        image: '/img/media/angellaunch.png'
+    },
+    {
+        title: 'Chainges',
+        url: 'https://www.youtube.com/watch?v=8kj5KPdrTuk',
+        image: '/img/media/chainges.png'
+    },
+    {
+        title: 'CryptoAcademy',
+        url: 'https://www.youtube.com/watch?v=34Luaslg8hs',
+        image: '/img/media/ca-nl.png'
+    },
+    {
+        title: 'DailyHODL',
+        url: 'https://dailyhodl.com/2018/02/16/what-is-ntitle/',
+        image: '/img/media/the-daily-hodl-logo.png'
+    },
+    {
+        title: 'Impact Tech',
+        url: 'https://www.youtube.com/watch?v=HEC2Jw8hJB0',
+        image: '/img/media/impacttech.png'
+    },
+    {
+        title: 'Dragon Valley',
+        url: 'https://www.youtube.com/watch?v=MKtkBhtTYHA',
+        image: '/img/media/dragonvalley.png'
+    },
+];
--- src/components/SectionNews/SectionNews.data.js
+++ src/components/SectionNews/SectionNews.data.js
@@ -1,62 +1,27 @@
 module.exports = [
     {
-        title: 'Game Developer Conference',
-        url: 'https://www.youtube.com/watch?v=YBijFXQFZrc',
-        image: '/img/media/gdc_logo.png',
+        title: 'Blockchainstartup nTitle gives back power to the game-developer and gamer',
+        url: 'https://www.emerce.nl/interviews/interview-vergeet-steam-nederlandse-blockchainstartup-ntitle-geeft-macht-terug-gameontwikkelaar-n-gamer',
+        image: '/img/media/emerce-news.jpg',
     },
     {
-        title: 'Blockchain Expo Global',
-        url: 'https://www.youtube.com/watch?v=yJ36deomX5c',
-        image: '/img/media/blockchain_expo_global.png',
+        title: 'nTitle signs Team Dutch Gaming esports sponsorship deal',
+        url: 'https://www.blockchaingamer.biz/news/5091/ntitle-signs-team-dutch-gaming-esports-sponsorship-deal/',
+        image: '/img/media/blockchain-gamer-news.jpg',
     },
     {
-        title: 'Blockchain Expo Europe',
-        url: '#',
-        image: '/img/media/blockchain_expo_europe.png',
-    },
-    {
-        title: 'Dagvandecrypto',
-        url: 'https://www.youtube.com/watch?v=zpevvx3VKhI',
-        image: '/img/media/dagvancrypto.png',
-    },
-    {
-        title: 'Angel Launch',
-        url: 'https://www.youtube.com/watch?v=J3gD84OfLNg',
-        image: '/img/media/angellaunch.png'
-    },
-    {
-        title: 'Chainges',
-        url: 'https://www.youtube.com/watch?v=8kj5KPdrTuk',
-        image: '/img/media/chainges.png'
+        title: 'nTitle announced new sponsorship deal with Team Dutch Gaming',
+        url: 'https://www.invenglobal.com/articles/5842/ntitle-announced-new-sponsorship-deal-with-team-dutch-gaming',
+        image: '/img/media/inven-global-news.jpg'
     },
     {
         title: 'Bitcoin Magazine NL',
         url: 'http://bitcoinmagazine.nl/en/2018/04/interview-vergeet-steam-de-nederlandse-blockchainstartup-ntitle-geeft-de-macht-terug-aan-de-game-ontwikkelaar-en-gamer/',
-        image: '/img/media/bcmag.png'
+        image: '/img/media/bcmag-news.jpg'
     },
     {
-        title: 'CryptoAcademy',
-        url: 'https://www.youtube.com/watch?v=34Luaslg8hs',
-        image: '/img/media/ca-nl.png'
-    },
-    {
-        title: 'DailyHODL',
-        url: 'https://dailyhodl.com/2018/02/16/what-is-ntitle/',
-        image: '/img/media/the-daily-hodl-logo.png'
-    },
-    {
-        title: 'Brave New World',
-        url: 'https://medium.com/@ntitle/ntitle-progress-update-may-24th-2977454d3ce6',
-        image: '/img/media/bnw.png'
-    },
-    {
-        title: 'Impact Tech',
-        url: 'https://www.youtube.com/watch?v=HEC2Jw8hJB0',
-        image: '/img/media/impacttech.png'
-    },
-    {
-        title: 'Dragon Valley',
-        url: 'https://www.youtube.com/watch?v=MKtkBhtTYHA',
-        image: '/img/media/dragonvalley.png'
-    },
+        title: 'nTitle Progress Update: July 26th',
+        url: 'https://medium.com/@ntitle/ntitle-progress-update-july-26th-5e818d3bcbcd',
+        image: '/img/media/ntitle-progress-update.jpg',
+    }
 ];
