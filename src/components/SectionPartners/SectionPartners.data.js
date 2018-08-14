23c5c3cf793dc3bc68436447abaf1f06eb9d7b08
Mark Vonk <markjhvonk@gmail.com>
2018-08-14 13:25:02 +0200

    Added partners section

---
+++ src/components/SectionPartners/SectionPartners.data.js
@@ -0,0 +1,37 @@
+module.exports = [
+    {
+        title: 'Utomik',
+        url: 'https://www.utomik.com/',
+        image: '/img/partners/Utomik.jpg',
+    },
+    {
+        title: 'Abstraction Games',
+        url: 'https://abstractiongames.com/',
+        image: '/img/partners/Abstraction-Games.jpg'
+    },
+    {
+        title: '3goo',
+        url: 'http://www.3goo.co.jp/',
+        image: '/img/partners/3goo.jpg'
+    },
+    {
+        title: '3rd Kind',
+        url: 'http://www.3rdkind-inc.com/',
+        image: '/img/partners/3rd-kind.jpg'
+    },
+    {
+        title: 'Tabletop Pixel',
+        url: 'http://www.tabletop-pixel.com/',
+        image: '/img/partners/Tabletop-pixel.jpg',
+    },
+    {
+        title: 'Dutch Games Association',
+        url: 'https://dutchgamesassociation.nl/',
+        image: '/img/partners/DGA.jpg',
+    },
+    {
+        title: 'JiJi Games',
+        url: 'http://www.jijigames.eu/',
+        image: '/img/partners/jiji-games.jpg',
+    }
+];
--- src/pages/index.data.js
+++ src/pages/index.data.js
@@ -4,6 +4,7 @@ const members = require('../components/SectionTeam/SectionTeam.Team.data');
 const advisors = require('../components/SectionTeam/SectionTeam.Advisors.data');
 const faq = require('../components/SectionFaq/SectionFaq.data');
 const news = require('../components/SectionNews/SectionNews.data');
+const partners = require('../components/SectionPartners/SectionPartners.data');
 const events = require('../components/SectionEvents/SectionEvents.data');
 const newsletter = require('../components/SectionNewsletter/SectionNewsletter.data');
 const social = require('../components/SectionCommunity/SectionCommunity.data');
@@ -17,6 +18,7 @@ const data = {
     benefits,
     faq,
     news,
+    partners,
     events,
     newsletter,
     social,
