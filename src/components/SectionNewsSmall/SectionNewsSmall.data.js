bad4c80502f4d7ed72cc6149d3bc0461af200c05
Mark Vonk <markjhvonk@gmail.com>
2018-08-17 10:37:02 +0200

    Added small news section

---
+++ src/components/SectionNewsSmall/SectionNewsSmall.data.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        title: 'Blockchainstartup nTitle gives back power to the game-developer and gamer',
+        url: 'https://www.emerce.nl/interviews/interview-vergeet-steam-nederlandse-blockchainstartup-ntitle-geeft-macht-terug-gameontwikkelaar-n-gamer',
+        image: '/img/media/emerce-news.jpg'
+    },
+    {
+        title: 'nTitle signs Team Dutch Gaming esports sponsorship deal',
+        url: 'https://www.blockchaingamer.biz/news/5091/ntitle-signs-team-dutch-gaming-esports-sponsorship-deal/',
+        image: '/img/media/blockchain-gamer-news.jpg'
+    },
+    {
+        title: 'nTitle announced new sponsorship deal with Team Dutch Gaming',
+        url: 'https://www.invenglobal.com/articles/5842/ntitle-announced-new-sponsorship-deal-with-team-dutch-gaming',
+        image: '/img/media/inven-global-news.jpg'
+    },
+    {
+        title: 'Bitcoin Magazine NL',
+        url: 'http://bitcoinmagazine.nl/en/2018/04/interview-vergeet-steam-de-nederlandse-blockchainstartup-ntitle-geeft-de-macht-terug-aan-de-game-ontwikkelaar-en-gamer/',
+        image: '/img/media/bcmag-news.jpg'
+    }
+];
