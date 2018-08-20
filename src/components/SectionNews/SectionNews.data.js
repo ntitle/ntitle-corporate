63ed0a62e913ce3871997c057a1a2a326b9b84c7
Mark Vonk <markjhvonk@gmail.com>
2018-08-20 20:23:51 +0200

    Added actual snippets

--- src/components/SectionNews/SectionNews.data.js
+++ src/components/SectionNews/SectionNews.data.js
@@ -1,42 +1,42 @@
 module.exports = [
     {
-        title: 'Blockchainstartup nTitle gives back power to the game-developer and gamer',
-        snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        title: 'Blockchainstartup nTitle geeft macht terug aan game-ontwikkelaar én gamer',
+        snippet: 'Een van de meest interessante blockchain startups van eigen bodem is nTitle uit Amsterdam. Oprichter en ceo Peter van Grinsven bouwde...',
         url: 'https://www.emerce.nl/interviews/interview-vergeet-steam-nederlandse-blockchainstartup-ntitle-geeft-macht-terug-gameontwikkelaar-n-gamer',
         image: '/img/media/emerce-news.jpg',
         date: '10-07-2018'
     },
     {
         title: 'How nTitle wants to shake up games distribution',
-        snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        snippet: 'The internet revolutionized how we buy games and the blockchain is going to revolutionize how we buy and sell games. No surprise...',
         url: 'https://www.blockchaingamer.biz/features/interviews/5430/ntitle-new-game-distribution-paradigm/',
         image: '/img/media/blockchain-gamer-news.jpg',
         date: '15-08-2018'
     },
     {
         title: 'nTitle announced new sponsorship deal with Team Dutch Gaming',
-        snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        snippet: 'nTitle, a game marketplace built on the Ethereum peer-to-peer network, announced their sponsorship deal with Team Dutch Gaming (TDG).',
         url: 'https://www.invenglobal.com/articles/5842/ntitle-announced-new-sponsorship-deal-with-team-dutch-gaming',
         image: '/img/media/inven-global-news.jpg',
         date: '01-08-2018'
     },
     {
         title: 'Bitcoin Magazine NL',
-        snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        snippet: 'Een van de meest interessante blockchain startups van eigen bodem is nTitle uit Amsterdam. Bitcoin Magazine NL sprak met oprichter...',
         url: 'http://bitcoinmagazine.nl/en/2018/04/interview-vergeet-steam-de-nederlandse-blockchainstartup-ntitle-geeft-de-macht-terug-aan-de-game-ontwikkelaar-en-gamer/',
         image: '/img/media/bcmag-news.jpg',
         date: '11-04-2018'
     },
     {
         title: 'DailyHODL',
-        snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        snippet: 'The founders of nTitle say they’re building a revolutionary gaming platform for buying, selling and promoting games. Built on...',
         url: 'https://dailyhodl.com/2018/02/16/what-is-ntitle/',
         image: '/img/media/the-daily-hodl-logo.png',
         date: '16-02-2018'
     },
     {
         title: 'nTitle Progress Update: July 26th',
-        snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        snippet: 'We’re excited to present our latest progress update! This time we are announcing our new website and additions to the team!',
         url: 'https://medium.com/@ntitle/ntitle-progress-update-july-26th-5e818d3bcbcd',
         image: '/img/media/ntitle-progress-update.jpg',
         date: '26-07-2018'
