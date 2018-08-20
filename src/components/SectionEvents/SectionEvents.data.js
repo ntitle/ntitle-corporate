3314c2dc4f9fe3558d1dc48f073dba12ec08e1ae
Mark Vonk <markjhvonk@gmail.com>
2018-08-20 20:23:32 +0200

    Added/updated locations and dates

--- src/components/SectionEvents/SectionEvents.data.js
+++ src/components/SectionEvents/SectionEvents.data.js
@@ -1,55 +1,55 @@
 module.exports = [
     {
         title: 'Game Developer Conference',
-        location: 'San Francisco',
+        location: 'San Francisco - 19-23 March 2018',
         url: 'https://www.youtube.com/watch?v=YBijFXQFZrc',
         image: '/img/media/gdc_logo.png',
     },
     {
         title: 'Blockchain Expo Global',
-        location: 'London',
+        location: 'London - 18-19 April 2018',
         url: 'https://www.youtube.com/watch?v=yJ36deomX5c',
         image: '/img/media/blockchain_expo_global.png',
     },
     {
         title: 'Blockchain Expo Europe',
-        location: '',
+        location: 'Amsterdam - 27-28 June 2018',
         url: '#',
         image: '/img/media/blockchain_expo_europe.png',
     },
     {
         title: 'Dagvandecrypto',
-        location: 'Amsterdam RAI',
+        location: 'Amsterdam - 29-30 January 2018',
         url: 'https://www.youtube.com/watch?v=zpevvx3VKhI',
         image: '/img/media/dagvancrypto.png',
     },
     {
         title: 'Angel Launch',
-        location: 'San Francisco',
+        location: 'San Francisco - 19-23 March 2018',
         url: 'https://www.youtube.com/watch?v=J3gD84OfLNg',
         image: '/img/media/angellaunch.png'
     },
     {
         title: 'Chainges',
-        location: 'Amsterdam',
+        location: 'Amsterdam - 4-5 May 2018',
         url: 'https://www.youtube.com/watch?v=8kj5KPdrTuk',
         image: '/img/media/chainges.png'
     },
     {
         title: 'CryptoAcademy',
-        location: '',
+        location: 'Amsterdam - 14 April',
         url: 'https://www.youtube.com/watch?v=34Luaslg8hs',
         image: '/img/media/ca-nl.png'
     },
     {
         title: 'Impact Tech',
-        location: 'Kraków, Poland',
+        location: 'Kraków, Poland - 13 February',
         url: 'https://www.youtube.com/watch?v=HEC2Jw8hJB0',
         image: '/img/media/impacttech.png'
     },
     {
         title: 'Dragon Valley',
-        location: 'Kraków, Poland',
+        location: 'Kraków, Poland - 13 June',
         url: 'https://www.youtube.com/watch?v=MKtkBhtTYHA',
         image: '/img/media/dragonvalley.png'
     },
