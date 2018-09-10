691e5be4aa4e10eb53a9d58774bf4be99a7ae063
Mark Vonk <markjhvonk@gmail.com>
2018-09-10 23:09:52 +0200

    Moved around news and event section, added partner

--- src/components/SectionEvents/SectionEvents.data.js
+++ src/components/SectionEvents/SectionEvents.data.js
@@ -1,15 +1,9 @@
 module.exports = [
     {
-        title: 'Game Developer Conference',
-        location: 'San Francisco - 19-23 March 2018',
-        url: 'https://www.youtube.com/watch?v=YBijFXQFZrc',
-        image: '/img/media/gdc_logo.png',
-    },
-    {
-        title: 'Blockchain Expo Global',
-        location: 'London - 18-19 April 2018',
-        url: 'https://www.youtube.com/watch?v=yJ36deomX5c',
-        image: '/img/media/blockchain_expo_global.png',
+        title: 'Gamescom',
+        location: 'Köln - 21-25 August',
+        url: 'https://medium.com/@ntitle/ntitle-progress-update-august-23rd-fe06eec4f9e9',
+        image: 'img/media/gamescom.png'
     },
     {
         title: 'Blockchain Expo Europe',
@@ -18,16 +12,10 @@ module.exports = [
         image: '/img/media/blockchain_expo_europe.png',
     },
     {
-        title: 'Dagvandecrypto',
-        location: 'Amsterdam - 29-30 January 2018',
-        url: 'https://www.youtube.com/watch?v=zpevvx3VKhI',
-        image: '/img/media/dagvancrypto.png',
-    },
-    {
-        title: 'Angel Launch',
-        location: 'San Francisco - 19-23 March 2018',
-        url: 'https://www.youtube.com/watch?v=J3gD84OfLNg',
-        image: '/img/media/angellaunch.png'
+        title: 'Dragon Valley',
+        location: 'Kraków, Poland - 13 June',
+        url: 'https://www.youtube.com/watch?v=MKtkBhtTYHA',
+        image: '/img/media/dragonvalley.png'
     },
     {
         title: 'Chainges',
@@ -36,10 +24,28 @@ module.exports = [
         image: '/img/media/chainges.png'
     },
     {
+        title: 'Blockchain Expo Global',
+        location: 'London - 18-19 April 2018',
+        url: 'https://www.youtube.com/watch?v=yJ36deomX5c',
+        image: '/img/media/blockchain_expo_global.png',
+    },
+    {
         title: 'CryptoAcademy',
         location: 'Amsterdam - 14 April',
         url: 'https://www.youtube.com/watch?v=34Luaslg8hs',
         image: '/img/media/ca-nl.png'
+    },
+    {
+        title: 'Game Developer Conference',
+        location: 'San Francisco - 19-23 March 2018',
+        url: 'https://www.youtube.com/watch?v=YBijFXQFZrc',
+        image: '/img/media/gdc_logo.png',
+    },
+    {
+        title: 'Angel Launch',
+        location: 'San Francisco - 19-23 March 2018',
+        url: 'https://www.youtube.com/watch?v=J3gD84OfLNg',
+        image: '/img/media/angellaunch.png'
     },
     {
         title: 'Impact Tech',
@@ -48,9 +54,9 @@ module.exports = [
         image: '/img/media/impacttech.png'
     },
     {
-        title: 'Dragon Valley',
-        location: 'Kraków, Poland - 13 June',
-        url: 'https://www.youtube.com/watch?v=MKtkBhtTYHA',
-        image: '/img/media/dragonvalley.png'
+        title: 'Dagvandecrypto',
+        location: 'Amsterdam - 29-30 January 2018',
+        url: 'https://www.youtube.com/watch?v=zpevvx3VKhI',
+        image: '/img/media/dagvancrypto.png',
     },
 ];
